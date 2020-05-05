f9ffa666e78cf0fe6128ab32f4a8b83ee016d7e8
[synnaf] <[f.vforsman@gmail.com]>
2020-05-05 21:27:02 +0200

    Search function almost working, but it doesn't replace old items with new

--- dist/main.0e24b174.js
+++ dist/main.0e24b174.js
@@ -134,212 +134,7 @@ function () {
 
 exports.Movie = Movie;
 ;
-},{}],"models/Service.ts":[function(require,module,exports) {
-"use strict"; //syftet är att hämta data från vårt OMDB API 
-// http://www.omdbapi.com/?apikey=65873bd2 + paramterar 
-//http://www.omdbapi.com/?apikey=65873bd2&s="Harry+Potter"  
-//Våra tjänster skall bara ha en sak att göra. 
-//Vår main skall agera som klister genom att använda tjänsten,
-// och sedan skapa det som behövs för att applikationen skall bli användbar. 
-
-var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
-  function adopt(value) {
-    return value instanceof P ? value : new P(function (resolve) {
-      resolve(value);
-    });
-  }
-
-  return new (P || (P = Promise))(function (resolve, reject) {
-    function fulfilled(value) {
-      try {
-        step(generator.next(value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function rejected(value) {
-      try {
-        step(generator["throw"](value));
-      } catch (e) {
-        reject(e);
-      }
-    }
-
-    function step(result) {
-      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
-    }
-
-    step((generator = generator.apply(thisArg, _arguments || [])).next());
-  });
-};
-
-var __generator = this && this.__generator || function (thisArg, body) {
-  var _ = {
-    label: 0,
-    sent: function sent() {
-      if (t[0] & 1) throw t[1];
-      return t[1];
-    },
-    trys: [],
-    ops: []
-  },
-      f,
-      y,
-      t,
-      g;
-  return g = {
-    next: verb(0),
-    "throw": verb(1),
-    "return": verb(2)
-  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
-    return this;
-  }), g;
-
-  function verb(n) {
-    return function (v) {
-      return step([n, v]);
-    };
-  }
-
-  function step(op) {
-    if (f) throw new TypeError("Generator is already executing.");
-
-    while (_) {
-      try {
-        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-        if (y = 0, t) op = [op[0] & 2, t.value];
-
-        switch (op[0]) {
-          case 0:
-          case 1:
-            t = op;
-            break;
-
-          case 4:
-            _.label++;
-            return {
-              value: op[1],
-              done: false
-            };
-
-          case 5:
-            _.label++;
-            y = op[1];
-            op = [0];
-            continue;
-
-          case 7:
-            op = _.ops.pop();
-
-            _.trys.pop();
-
-            continue;
-
-          default:
-            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
-              _ = 0;
-              continue;
-            }
-
-            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
-              _.label = op[1];
-              break;
-            }
-
-            if (op[0] === 6 && _.label < t[1]) {
-              _.label = t[1];
-              t = op;
-              break;
-            }
-
-            if (t && _.label < t[2]) {
-              _.label = t[2];
-
-              _.ops.push(op);
-
-              break;
-            }
-
-            if (t[2]) _.ops.pop();
-
-            _.trys.pop();
-
-            continue;
-        }
-
-        op = body.call(thisArg, _);
-      } catch (e) {
-        op = [6, e];
-        y = 0;
-      } finally {
-        f = t = 0;
-      }
-    }
-
-    if (op[0] & 5) throw op[1];
-    return {
-      value: op[0] ? op[1] : void 0,
-      done: true
-    };
-  }
-};
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var Movie_1 = require("./Movie");
-
-var Service =
-/** @class */
-function () {
-  function Service() {} //här inne utvecklar vi funktionen och det som ska hända
-
-
-  Service.prototype.getData = function () {
-    return __awaiter(this, void 0, Promise, function () {
-      var moviefetch, moviedata, movies;
-      return __generator(this, function (_a) {
-        switch (_a.label) {
-          case 0:
-            console.log("hej jag hämtar data!");
-            return [4
-            /*yield*/
-            , fetch("http://www.omdbapi.com/?apikey=65873bd2&s=Harry+Potter")];
-
-          case 1:
-            moviefetch = _a.sent();
-            return [4
-            /*yield*/
-            , moviefetch.json()];
-
-          case 2:
-            moviedata = _a.sent();
-            movies = moviedata.Search.map(function (m) {
-              //för varje objekt m, skapa ett nytt objekt enligt movie-klassen: 
-              var newMovie = new Movie_1.Movie();
-              newMovie.id = m.imdbID;
-              newMovie.title = m.Title;
-              newMovie.releaseYear = m.Year;
-              newMovie.poster = m.Poster; //vi skickar tillbaka objektet i slutet av loopen
-
-              return newMovie;
-            }); //vi skickar tillbaka hela movies-objektet 
-
-            return [2
-            /*return*/
-            , movies];
-        }
-      });
-    });
-  };
-
-  return Service;
-}();
-
-exports.Service = Service;
-},{"./Movie":"models/Movie.ts"}],"models/Search.ts":[function(require,module,exports) {
+},{}],"models/Search.ts":[function(require,module,exports) {
 "use strict"; //vi testar att göra en modul för vår sökfunktion här 
 
 var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
@@ -696,25 +491,19 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var Service_1 = require("../models/Service");
-
 var Search_1 = require("../models/Search"); // import {MockService} from "../models/MockService"; 
 //för att köra igång applikationen 
 
 
 window.onload = function () {
-  //vi skapar upp en instans av Service, servide är det som implementerar interafcet 
-  //instansen blir av typen IDataService 
-  var service = new Service_1.Service();
-  var main = new Main(); //här kör vi funktionen i main-klassen!
-
-  main.start(service); //lyssna efter sökning, om sökning görs: 
-
+  //lyssna efter sökning, om sökning görs: 
   document.getElementById("searchButton").addEventListener("click", function () {
-    console.log(" you are searching...");
+    //vi skapar upp en instans av Service, servide är det som implementerar interafcet 
+    //instansen blir av typen IDataService 
     var service = new Search_1.Search();
-    var searchFor = new SearchC();
-    searchFor.search(service);
+    var main = new Main(); //här kör vi funktionen i main-klassen!
+
+    main.start(service);
   });
 }; //vi definierar klassen main
 //här inne kör vi funktionen getData(), som hämtar data från APIt. Här behöver vi plocka upp datan! 
@@ -727,30 +516,11 @@ function () {
 
   Main.prototype.start = function (service) {
     return __awaiter(this, void 0, void 0, function () {
-      return __generator(this, function (_a) {
-        console.log("Starting operation");
-        return [2
-        /*return*/
-        ];
-      });
-    });
-  };
-
-  return Main;
-}();
-
-var SearchC =
-/** @class */
-function () {
-  function SearchC() {} //sökfunktionen? 
-
-
-  SearchC.prototype.search = function (service) {
-    return __awaiter(this, void 0, void 0, function () {
       var movieList;
       return __generator(this, function (_a) {
         switch (_a.label) {
           case 0:
+            console.log("Starting operation");
             return [4
             /*yield*/
             , service.getData()];
@@ -784,9 +554,33 @@ function () {
     });
   };
 
-  return SearchC;
-}();
-},{"../models/Service":"models/Service.ts","../models/Search":"models/Search.ts"}],"../../../../.npm/_npx/11228/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+  return Main;
+}(); // class SearchC {
+//     //sökfunktionen? 
+//     async search(service: IService) {
+//         let movieList: Movie[] = await service.getData(); 
+//         console.log(movieList); 
+//         //hit funkar det! 
+//         //problemet nu är att den inte vet vad den ska göra när den returnerar 0 
+//         //för varje objekt i listan vill vi ersätta de gamla divvarna!
+//         movieList.forEach(mObject => {
+//             let container = document.getElementById("movie-container");
+//             let newText = document.createElement("div");
+//             newText.setAttribute("class", "movielist"); 
+//             container.appendChild(newText);
+//             let movietitle = document.createElement("h2"); 
+//             movietitle.innerHTML = mObject.title; 
+//             newText.appendChild(movietitle); 
+//             let movieYear = document.createElement("p"); 
+//             movieYear.innerHTML = "It was released: " + mObject.releaseYear; 
+//             newText.appendChild(movieYear); 
+//             let poster = document.createElement("img"); 
+//             poster.src = mObject.poster; 
+//             newText.appendChild(poster); 
+//         });
+//     }
+// }
+},{"../models/Search":"models/Search.ts"}],"../../../../.npm/_npx/11228/lib/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
--- ts/main.ts
+++ ts/main.ts
@@ -8,21 +8,15 @@ import {Search} from "../models/Search";
 //för att köra igång applikationen 
 window.onload = function() {
 
-    //vi skapar upp en instans av Service, servide är det som implementerar interafcet 
-    //instansen blir av typen IDataService 
-    let service: IService = new Service(); 
-    let main: Main = new Main(); 
-    //här kör vi funktionen i main-klassen!
-    main.start(service); 
-
-
     //lyssna efter sökning, om sökning görs: 
     document.getElementById("searchButton").addEventListener("click", ()=> {
 
-        console.log(" you are searching...")
-        let service: IService = new Search(); 
-        let searchFor: Search = new SearchC(); 
-        searchFor.search(service); 
+    //vi skapar upp en instans av Service, servide är det som implementerar interafcet 
+    //instansen blir av typen IDataService 
+    let service: IService = new Search(); 
+    let main: Main = new Main(); 
+    //här kör vi funktionen i main-klassen!
+    main.start(service); 
 
     })
 
@@ -35,41 +29,6 @@ class Main {
 
     async start(service: IService) {
         console.log("Starting operation"); 
-
-        // let movieList: Movie[] = await service.getData(); 
-        // console.log(movieList); 
-
-        // //för varje objekt i listan vill vi skapa upp en ny div 
-        // movieList.forEach(mObject => {
-
-        //     let container = document.getElementById("movie-container");
-        //     let newText = document.createElement("div");
-        //     newText.setAttribute("class", "movielist"); 
-        //     container.appendChild(newText);
-         
-        //     let movietitle = document.createElement("h2"); 
-        //     movietitle.innerHTML = mObject.title; 
-        //     newText.appendChild(movietitle); 
-
-
-        //     let movieYear = document.createElement("p"); 
-        //     movieYear.innerHTML = "It was released: " + mObject.releaseYear; 
-        //     newText.appendChild(movieYear); 
-
-        //     let poster = document.createElement("img"); 
-        //     poster.src = mObject.poster; 
-        //     newText.appendChild(poster); 
-         
-        // });
-
-    }
-
-}
-
-class SearchC {
-
-    //sökfunktionen? 
-    async search(service: IService) {
 
         let movieList: Movie[] = await service.getData(); 
         console.log(movieList); 
@@ -101,4 +60,40 @@ class SearchC {
     }
 
 }
+
+// class SearchC {
+
+//     //sökfunktionen? 
+//     async search(service: IService) {
+
+//         let movieList: Movie[] = await service.getData(); 
+//         console.log(movieList); 
+//         //hit funkar det! 
+//         //problemet nu är att den inte vet vad den ska göra när den returnerar 0 
+//         //för varje objekt i listan vill vi ersätta de gamla divvarna!
+//         movieList.forEach(mObject => {
+
+//             let container = document.getElementById("movie-container");
+//             let newText = document.createElement("div");
+//             newText.setAttribute("class", "movielist"); 
+//             container.appendChild(newText);
+         
+//             let movietitle = document.createElement("h2"); 
+//             movietitle.innerHTML = mObject.title; 
+//             newText.appendChild(movietitle); 
+
+
+//             let movieYear = document.createElement("p"); 
+//             movieYear.innerHTML = "It was released: " + mObject.releaseYear; 
+//             newText.appendChild(movieYear); 
+
+//             let poster = document.createElement("img"); 
+//             poster.src = mObject.poster; 
+//             newText.appendChild(poster); 
+
+//         });
+
+//     }
+
+// }
 
